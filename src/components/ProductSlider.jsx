032f331a1ad04297f15140754328a6c9203c1f56
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-07-14 23:55:22 +0600

    product slider complete

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -2,7 +2,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import 'swiper/swiper.scss';
-
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
@@ -19,8 +18,8 @@ const ProductSlider = () => {
   .then(data=>setData(data))
   },[])
     return (
-        <div className='w-full mb-8'>
-          <h3>Trending Now</h3>
+        <div className='w-full my-8 swiper-container'>
+          <h3 className='font-medium uppercase text-2xl absolute'>Trending Now</h3>
       <Swiper
       spaceBetween={5}
       slidesPerView={5}
@@ -28,8 +27,8 @@ const ProductSlider = () => {
     >
       {
         data.map(product=><SwiperSlide key={product.id}>
-          <div className='inline-block border flex justify-center flex-col w-48'>
-          <Image className='mx-auto' src={product.image} alt="" width={100} height={200} />
+          <div className='border flex justify-center flex-col w-48 mt-12 bg-gray-100 p-2'>
+          <Image className='mx-auto bg-gray-100' src={product.image} alt="" width={150} height={300} />
           <p>{product.productName}</p>
           <p className='text-gray-400 text-xs'>{product.variant}</p>
           <h6 className='font-medium text-[#008ECC]'><span className='text-xl'>&#2547;</span>{product.price}</h6>
