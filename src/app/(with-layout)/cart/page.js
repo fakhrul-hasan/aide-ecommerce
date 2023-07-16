140063a96b978c0f5ea46f201467bf268cf9540a
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-07-17 04:28:12 +0600

    useRouter changed

--- src/app/(with-layout)/cart/page.js
+++ src/app/(with-layout)/cart/page.js
@@ -1,11 +1,9 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import Cart from '@/components/Cart';
 import UseAuth from '@/hooks/useAuth';
 
 const page = () => {
     const {user, loading} = UseAuth();
-    const router = useRouter();
     if(loading){
         return <div className='mx-auto font-medium text-lg'>Loading...</div>
     }
@@ -15,7 +13,7 @@ const page = () => {
         )
     }
     if (typeof window !== 'undefined') {
-        router.push('/signIn');
+        return window.location.href = '/signIn';
       }
     
       return null;
